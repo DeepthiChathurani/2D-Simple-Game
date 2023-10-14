6d3984f8a0ac357c0c2229e13fec6fbb72ca9a33
deepthichathurani64@gmail.com <deepthichathurani64@gmail.com>
2023-10-14 10:20:58 +0530

    level change

--- src/lk.ijse.2dgame/script4.js
+++ src/lk.ijse.2dgame/script4.js
@@ -11,6 +11,9 @@ var deadSound = new Audio("assets/audio/dead.mp3");
 //level up sound
 var levelup =new Audio("assets/audio/levelup.mp3");
 
+//slide sound
+var slideSound =new Audio("assets/audio/slide.mp3");
+
 function keyCheck(event) {
     //Enter
     if (event.which == 13) {
@@ -24,6 +27,7 @@ function keyCheck(event) {
             scoreWorkerId = setInterval(updateScore, 100);
 
             moveBlockWorkerId = setInterval(moveBlock, 100);
+            moveDragonWorkerId = setInterval(moveDragon,100);
         }
 
     }
@@ -34,14 +38,28 @@ function keyCheck(event) {
         if (jumpWorkerId == 0) {
 
             clearInterval(runWorkerId);
+            clearInterval(slideWorkerId);
             runSound.pause();
             jumpWorkerId = setInterval(jump, 100);
             jumpSound.play();
         }
     }
+    // down arrow
+    if(event.which == 40){
+        if (slideWorkerId == 0) {
+
+            clearInterval(runWorkerId);
+            clearInterval(jumpWorkerId);
+            runSound.pause();
+            slideWorkerId = setInterval(slide, 100);
+            slideSound.play();
+        }
+    }
 }
+
+
 //run
-var boyId = document.getElementById("boy2");
+var boyId = document.getElementById("boy");
 var runImageNumber = 1;
 var runWorkerId = 0;
 
@@ -57,7 +75,7 @@ function run() {
 //jump
 var jumpImageNumber = 1;
 var jumpWorkerId = 0;
-var boyMarginTop = 314;
+var boyMarginTop = 355;
 
 function jump() {
     jumpImageNumber++;
@@ -89,13 +107,56 @@ function jump() {
         if (moveBlockWorkerId == 0) {
             moveBlockWorkerId = setInterval(moveBlock, 100);
         }
+        if (moveDragonWorkerId == 0) {
+            moveDragonWorkerId = setInterval(moveDragon, 100);
+        }
     }
 
     boyId.src = "assets/img/Jump (" + jumpImageNumber + ").png"
 }
 
+//slide
+var slideWorkerId=0;
+var slideImageNumber =0;
+
+function slide() {
+    slideImageNumber = slideImageNumber+1;
+    if(slideImageNumber >=1 & slideImageNumber <=5){
+        boyMarginTop = boyMarginTop +8;
+        boyId.style.marginTop = boyMarginTop + "px";
+    }
+    if(slideImageNumber >=6 & slideImageNumber <10){
+        boyMarginTop = boyMarginTop -8;
+        boyId.style.marginTop = boyMarginTop + "px";
+    }
+    if(slideImageNumber==11){
+        slideImageNumber =1;
+
+        clearInterval(slideWorkerId);
+        slideWorkerId=0;
+
+        runWorkerId = setInterval(run,100);
+        runSound.play();
+
+
+        if (backgroundWorkerId == 0) {
+            backgroundWorkerId = setInterval(moveBackground, 100);
+        }
+        if (scoreWorkerId == 0) {
+            scoreWorkerId = setInterval(updateScore, 100);
+        }
+        if (moveBlockWorkerId == 0) {
+            moveBlockWorkerId = setInterval(moveBlock, 100);
+        }
+        if (moveDragonWorkerId == 0) {
+            moveDragonWorkerId = setInterval(moveDragon, 100);
+        }
+    }
+    boyId.src ="assets/img/Slide (" + slideImageNumber + ").png";
+}
+
 // Move Background
-var BackgroundId = document.getElementById("background3");
+var BackgroundId = document.getElementById("background4");
 var backgroundX = 0;
 var backgroundWorkerId = 0;
 
@@ -105,35 +166,31 @@ function moveBackground() {
 }
 
 //score
-var scoreId = document.getElementById("score");
+
+var scoreId = document.getElementById("score4");
 var newScore = 0;
 var scoreWorkerId = 0;
 
 function updateScore() {
-    newScore = newScore + 2;
+    newScore = newScore + 4;
     scoreId.innerHTML = newScore;
-    if(newScore==500){
+    if(newScore==700){
         levelCompleted();
     }
-
 }
-var blockMarginLeft =650;
 
+//block create
+
+var blockMarginLeft =650;
 function createBlock(){
     for(var i=0; i<= 100 ;i++){
         var block = document.createElement("div");
         block.className="block3";
-        document.getElementById("background3").appendChild(block);
+        document.getElementById("background4").appendChild(block);
         block.style.marginLeft = blockMarginLeft +"px";
         block.id ="block3"+i;
+        blockMarginLeft =blockMarginLeft +500;
 
-     //   var gap = Math.random() * (1000 - 400) + 400;
-        if(i<=10){
-            blockMarginLeft =blockMarginLeft +350;
-        }
-        if(i>=11){
-            blockMarginLeft =blockMarginLeft +270;
-        }
     }
 }
 //block move
@@ -154,13 +211,64 @@ function moveBlock() {
             // alert("Dead");
             // 305
 
-            if (boyMarginTop > 305) {
+            if (boyMarginTop > 345) {
                 clearInterval(runWorkerId);
                 runSound.pause();
                 clearInterval(jumpWorkerId);
                 jumpWorkerId = -1;
                 clearInterval(scoreWorkerId);
                 clearInterval(backgroundWorkerId);
+                clearInterval(moveDragonWorkerId)
+                clearInterval(moveBlockWorkerId);
+
+                deadWorkerId = setInterval(dead, 100);
+                deadSound.play();
+            }
+        }
+    }
+}
+// dragon block create
+
+var dragonMarginTop =345;
+var dragonMarginLeft = 900;
+
+function createDragon() {
+    for (var i=0; i<100; i++){
+        var dragonGif = document.createElement("div");
+        dragonGif.className="dragon";
+        document.getElementById("background4").appendChild(dragonGif);
+        dragonGif.style.marginLeft = dragonMarginLeft +"px";
+        dragonGif.id ="dragon"+i;
+        dragonMarginLeft =dragonMarginLeft +500;
+    }
+}
+//move dragon
+var moveDragonWorkerId = 0;
+
+function moveDragon() {
+    for (var i=0; i<100; i++){
+        var newDragon = document.getElementById("dragon"+i);
+        var currentDragonMarginLeft = getComputedStyle(newDragon).marginLeft;
+        var newDragonMarginLeft = parseInt(currentDragonMarginLeft) - 20;
+
+        newDragon.style.marginLeft = newDragonMarginLeft + "px";
+
+        if (newDragonMarginLeft < 120 & newDragonMarginLeft >10) {
+
+
+            if (boyMarginTop <= 370) {
+                clearInterval(runWorkerId);
+                runWorkerId =-1;
+                runSound.pause();
+                clearInterval(jumpWorkerId);
+                jumpWorkerId = -1;
+                jumpSound.pause()
+                clearInterval(slideWorkerId);
+                slideWorkerId=-1;
+                slideSound.pause();
+                clearInterval(scoreWorkerId);
+                clearInterval(backgroundWorkerId);
+                clearInterval(moveDragonWorkerId)
                 clearInterval(moveBlockWorkerId);
 
                 deadWorkerId = setInterval(dead, 100);
@@ -204,6 +312,8 @@ function levelCompleted(){
     backgroundWorkerId =-1;
     clearInterval(moveBlockWorkerId);
     moveBlockWorkerId=-1;
+    clearInterval(moveDragonWorkerId);
+    moveDragonWorkerId=-1;
 
     document.getElementById("nextLevel1").style.visibility = "visible";
     document.getElementById("currentScore").style.visibility = "visible";
